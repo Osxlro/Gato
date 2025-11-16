a7bd2dd0ccba133aec3ab5f8c158f51b1f787b98
Oscurin <dominicoscuro@hotmail.com>
2025-11-16 13:39:41 -0600

    io.h changes

--- src/io.h
+++ src/io.h
@@ -18,26 +18,4 @@ void showRanking(const PlayerRecord arr[], int n);
 int  upsertResult(const char *name, int wins, int draws, int losses, int score);
 int  saveAllRanking(const PlayerRecord arr[], int n, const char *filePath);
 
-
-void sortRankingDesc(PlayerRecord arr[], int n) {
-    // Ordenamiento simple por puntaje descendente, desempate por nombre ascendente
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j + 1 < n; ++j) {
-            int swap = 0;
-            if (arr[j].score < arr[j+1].score) {
-                swap = 1;
-            } else if (arr[j].score == arr[j+1].score) {
-                // desempate por nombre ascendente
-                if (strcmp(arr[j].name, arr[j+1].name) > 0) swap = 1;
-            }
-            if (swap) {
-                PlayerRecord tmp = arr[j];
-                arr[j]   = arr[j+1];
-                arr[j+1] = tmp;
-            }
-        }
-    }
-}
-
-
 #endif
